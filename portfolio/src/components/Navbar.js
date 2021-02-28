b6f52c09d2239b249917c334e79589b40ce5c346
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-02-28 18:50:18 -0500

    Adding more styles, responsive behavior and favicon

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,20 +4,34 @@ import logo from "../mh-logo.png"
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img className="logo rounded" src={logo} alt="logo..."/></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div>
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#"><img className="logo rounded" src={logo} alt="logo..."/></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">Link</a>
-              </li>
-            </ul>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">about me</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">how work</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">portfolio</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">contacts</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
