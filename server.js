f2b2031080cb26bdd391bfad57d983ddc5a3c40f
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-03-12 22:00:14 -0500

    Deploy attempt

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 // Send every request to the React app
 // Define any API routes before this runs
+
+
+
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
